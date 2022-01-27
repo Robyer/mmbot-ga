2490bcdee3212fc4cfdd7103a4b5c500ede1c774
Robert Pösel <robyer@seznam.cz>
2022-01-28 00:08:30 +0100

    Revert "Revert splits (just for test)"
    This reverts commit 35a507e1143e4fe8c55a5d6a4e4c8949a873f404.

--- MMBotGA/data/provider/FixedDataProvider.cs
+++ MMBotGA/data/provider/FixedDataProvider.cs
@@ -19,7 +19,9 @@ namespace MMBotGA.data.provider
             Allocations = AllocationDefinitions.Select(x => x.ToAllocation()).ToArray(),
             DateSettings = new DataProviderDateSettings
             {
-                Automatic = true
+                Automatic = true,
+                Backtest = DateTimeRange.FromDiff(DateTime.UtcNow.Date.AddDays(-60), TimeSpan.FromDays(-365)),
+                Control = DateTimeRange.FromDiff(new DateTime(2022, 1, 4, 0,0,0, DateTimeKind.Utc), TimeSpan.FromDays(-60))
             }
         };
 
@@ -54,17 +56,70 @@ namespace MMBotGA.data.provider
                 ? DateTimeRange.FromDiff(DateTime.UtcNow.Date.AddDays(-60), TimeSpan.FromDays(-365))
                 : Settings.DateSettings.Backtest;
 
-            var diff = backtestRange.End - backtestRange.Start;
-            var totalMinutes = (int)diff.TotalMinutes;
+            const int splits = 3;
 
             return Settings.Allocations
-                .Select(x => new Batch(x.ToBatchName(),
-                    new[]
+                .Select(x =>
+                {
+                    var file = downloader.Download(new DownloadTask(DataFolder, x.Exchange, x.Symbol, backtestRange));
+                    return new
                     {
-                        downloader.GetBacktestData(x, DataFolder, backtestRange, false, backtestRange.Start, totalMinutes, 0),
-                        downloader.GetBacktestData(x, DataFolder, backtestRange, true, backtestRange.Start, totalMinutes, 0)
-                    }))
+                        Allocation = x,
+                        File = file,
+                        Size = File.ReadAllLines(file).Length
+                    };
+                })
+                .Where(x => x.Size > 100)
+                .Select(x =>
+                {
+                    var partMinutes = x.Size / splits;
+                    var halfPartMinutes = partMinutes / 2;
+
+                    var offsets = Enumerable
+                        .Repeat(partMinutes, splits)
+                        .Select((p, i) => p * i)
+                        .Concat(Enumerable
+                            .Repeat(partMinutes, splits - 1)
+                            .Select((p, i) => halfPartMinutes + p * i)
+                        );
+
+                    return new Batch(x.Allocation.ToBatchName(),
+                        offsets
+                            .Select(o => new BacktestData
+                            {
+                                Broker = x.Allocation.Exchange.ToLower(),
+                                Pair = x.Allocation.RobotSymbol ??
+                                       x.Allocation.Symbol, // Get broker pair info: /admin/api/brokers/kucoin/pairs
+                                SourceFile = x.File,
+                                Reverse = false,
+                                Balance = x.Allocation.Balance,
+                                Start = null,
+                                Limit = partMinutes,
+                                Offset = o
+                            })
+                            .ToArray()
+                    );
+                })
                 .ToArray();
+
+            //var partDays = (int)diff.TotalDays / splits;
+            //var overlapStart = backtestRange.Start.AddDays(partDays / 2);
+            //var parts = Enumerable
+            //    .Repeat(partDays, splits)
+            //    .Select((p, i) => DateTimeRange.FromDiff(backtestRange.Start.AddDays(p * i), TimeSpan.FromDays(p)))
+            //    .Concat(Enumerable
+            //        .Repeat(partDays, splits - 1)
+            //        .Select((p, i) => DateTimeRange.FromDiff(overlapStart.AddDays(p * i), TimeSpan.FromDays(p)))
+            //    );
+
+            //return Settings.Allocations
+            //    .Select(x => new Batch(x.ToBatchName(),
+            //        new[]
+            //        {
+            //            downloader.GetBacktestData(x, DataFolder, backtestRange, false),
+            //            downloader.GetBacktestData(x, DataFolder, backtestRange, true)
+            //        }))
+            //    .ToArray();
         }
 
         public Batch[] GetControlData(IProgress progressCallback)
@@ -74,14 +129,11 @@ namespace MMBotGA.data.provider
                 ? DateTimeRange.FromUtcToday(TimeSpan.FromDays(-60))
                 : Settings.DateSettings.Control;
 
-            var diff = controlRange.End - controlRange.Start;
-            var totalMinutes = (int)diff.TotalMinutes;
-
             return Settings.Allocations
                 .Select(x => new Batch(x.ToBatchName(),
                     new[]
                     {
-                        downloader.GetBacktestData(x, DataFolder, controlRange, false, controlRange.Start, totalMinutes, 0)
+                        downloader.GetBacktestData(x, DataFolder, controlRange, false)
                     }))
                 .ToArray();
         }
